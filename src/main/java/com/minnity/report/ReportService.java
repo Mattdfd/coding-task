421a07a2c2bcd26d634c0319a8c0079a5d2f2326
Matthew Mikhaiel <MattDFD@live.ca>
2024-01-11 15:11:53 +0100

    3 tasks and 3 tests complete

--- src/main/java/com/minnity/report/ReportService.java
+++ src/main/java/com/minnity/report/ReportService.java
@@ -2,23 +2,63 @@ package com.minnity.report;
 
 import org.apache.commons.lang3.NotImplementedException;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ReportService {
 
   //task 1: Return number of requests that were made for each company. (e.g. companyId -> requestNumber)
   public Map<Integer, Long> calculateNumberOfRequestsPerCompany(List<RequestLog> requestLogs) {
-    throw new NotImplementedException("TODO implement this method");
+    //initialize Map
+    Map<Integer, Long> numberOfRequestsPerCompany =new HashMap<>();
+
+    //Loop through the requestLogs, check if map contains companyid, if it does add 1 to the current count, if not add 1
+    for (RequestLog log : requestLogs) {
+      int companyId = log.getCompanyId();
+      if (numberOfRequestsPerCompany.containsKey(companyId)) {
+        long currentCount = numberOfRequestsPerCompany.get(companyId);
+        numberOfRequestsPerCompany.put(companyId, currentCount + 1);
+      } else {
+        numberOfRequestsPerCompany.put(companyId, 1L);
+      }
+
+    }
+    return numberOfRequestsPerCompany;
   }
 
   //task 2: Count and return requests per company that finished with an error HTTP response code (>=400)
-  public Map<Integer, RequestLog> findRequestsWithError(List<RequestLog> requestLogs) {
-    throw new NotImplementedException("TODO implement this method");
+  public Map<Integer, List<RequestLog>> findRequestsWithError(List<RequestLog> requestLogs) {
+    Map<Integer, List<RequestLog>> requestsWithError =new HashMap<>();
+
+    //loop through the request logs, and check for request status' of >=400
+    for (RequestLog log : requestLogs){
+      if(log.getRequestStatus()>=400){
+        int companyId = log.getCompanyId();
+        List<RequestLog> logsWithError = requestsWithError.get(companyId);
+
+        //if the list doesnt exist for the company create a new list and add the list to the map requestsWithError
+        if(logsWithError ==null){
+          logsWithError = new ArrayList<>();
+          requestsWithError.put(companyId,logsWithError);
+        }
+        //add the log with error to the list
+        logsWithError.add(log);
+      }
+    }
+    return requestsWithError;
   }
 
   //task 3: find and print API (requests path) that on average takes the longest time to process the request.
   public String findRequestPathWithLongestDurationTime(List<RequestLog> requestLogs) {
-    throw new NotImplementedException("TODO implement this method");
+
+    //map holds the groups of requestpaths and their calculated avg of duration of requests
+    Map<String,Double> pathAndDurationMap =
+            requestLogs.stream().collect(Collectors.groupingBy(RequestLog::getRequestPath, Collectors.averagingDouble(RequestLog::getRequestDuration)));
+
+    //another map to hold the highest avg value in the pathAndDurationMap
+    Optional<Map.Entry<String, Double>> maxEntry = pathAndDurationMap.entrySet().stream().max(Comparator.comparingDouble(Map.Entry::getValue));
+
+    //returning the key or path for the highest avg request duration
+    return maxEntry.map(Map.Entry::getKey).orElse(null);
   }
 }
--- src/test/java/com/minnity/report/ReportServiceTest.java
+++ src/test/java/com/minnity/report/ReportServiceTest.java
@@ -2,11 +2,106 @@ package com.minnity.report;
 
 import org.junit.Test;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class ReportServiceTest {
 
   @Test
-  public void todo() {
-    RequestLog sampleRequest = SampleDataGenerator.aRequestLog();
+  public void testCalculateNumberOfRequestsPerCompanyWithRandomData() {
+
+    //using SampleDataGenerator to create data
+    List<RequestLog> logs = new ArrayList<>();
+    for (int i = 0; i < 50; i++) {
+      logs.add(SampleDataGenerator.aRequestLog());
+    }
+
+    ReportService reportService = new ReportService();
+
+    //call task1 method for testing
+    Map<Integer, Long> requestCount = reportService.calculateNumberOfRequestsPerCompany(logs);
+
+    int expectedCountForCompany = 0;
+
+    for (RequestLog log : logs) {
+      if (log.getCompanyId() == 4) {
+        expectedCountForCompany++;
+      }
+    }
+    System.out.println("Expected count: "+expectedCountForCompany);
+
+    //testing to see if the expected number of requests for company with ID 4 matches the actual count.
+    assertEquals(expectedCountForCompany, requestCount.getOrDefault(4, 0L).intValue());
+
+    System.out.println("Actual count: "+ requestCount.getOrDefault(4,0L).intValue());
   }
-}
+
+
+  @Test
+  public void testFindRequestsWithError(){
+
+    //using SampleDataGenerator to create data
+    List<RequestLog> logs = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      logs.add(SampleDataGenerator.aRequestLog());
+    }
+
+    ReportService reportService = new ReportService();
+
+    //call task2 method for testing
+    Map<Integer, List<RequestLog>> requestLogsMap = reportService.findRequestsWithError(logs);
+    System.out.println(requestLogsMap.toString());
+
+    //iterate over the logs and calculate the expected count for each company with an error status
+    Map<Integer, Long> expectedCountPerCompany = logs.stream().filter(log -> log.getRequestStatus() >= 400)
+            .collect(Collectors.groupingBy(RequestLog::getCompanyId, Collectors.counting()));
+
+    System.out.println(expectedCountPerCompany.toString());
+
+    int companyIdToTest = 4;
+
+    //testing for individual company using companyId.  gets value for the key.
+    long expectedCount = expectedCountPerCompany.getOrDefault(companyIdToTest, 0L);
+
+    List<RequestLog> requestLogsForCompany = requestLogsMap.getOrDefault(companyIdToTest, new ArrayList<>());
+
+    //filters request logs for company by >=400 to get actual count
+    long actualCount = requestLogsForCompany.stream().filter(log -> log.getRequestStatus() >= 400).count();
+
+    assertEquals(expectedCount, actualCount);
+    System.out.println("Expected: " + expectedCount + "\n" + "Actual: " + actualCount);
+
+  }
+
+  @Test
+  public void TestFindRequestPathWithLongestDurationTime() {
+
+    //using SampleDataGenerator to create data
+    List<RequestLog> logs = new ArrayList<>();
+    for (int i = 0; i < 20; i++) {
+      logs.add(SampleDataGenerator.aRequestLog());
+    }
+
+    //map holds the groups of requestpaths and their calculated avg of duration of requests
+    Map<String, Double> pathAndDurationMap =
+            logs.stream().collect(Collectors.groupingBy(RequestLog::getRequestPath,Collectors.averagingDouble(RequestLog::getRequestDuration)));
+
+    System.out.println(pathAndDurationMap.toString());
+    ReportService reportService = new ReportService();
+
+    //another map to hold the highest avg value in the pathAndDurationMap
+    Optional<Map.Entry<String, Double>> maxEntry = pathAndDurationMap.entrySet().stream().max(Comparator.comparingDouble(Map.Entry::getValue));
+
+    //expected value comes from the calculated avg duration from sample data set from above
+    String expected =maxEntry.map(Map.Entry::getKey).orElse(null);
+
+    //actual value comes from the method in ReportService
+    String actual = reportService.findRequestPathWithLongestDurationTime(logs);
+
+    assertEquals(expected,actual);
+    System.out.println("Expected: " +expected+ "\n"+ "Actual: "+ actual);
+  }
+  }
